019377c8a1f9f187ebecd0d7220d61a19d6b8cf7
Andrea <rakosfalvyandrea@gmail.com>
2021-08-04 20:24:11 +0200

    fe ba communication practice

--- back-end/server.js
+++ back-end/server.js
@@ -1,17 +1,46 @@
 const express = require('express')
 const path = require('path')
 const server = express();
+//const bodyParser = require('body-parser');
+let multer  = require('multer');
+const { runInNewContext } = require('vm');
+
+let upload  = multer({ storage: multer.memoryStorage() });
+
+// server.use(bodyParser.json());
+// server.use(bodyParser.urlencoded({ extended: true }));
+
 
 server.get('/', (req, res) => {
-    res.sendFile( 
-        path.join(__dirname, '../front-end/index.html')
+  res.sendFile( 
+    path.join(__dirname, '../front-end/index.html')
     )
-});
+  });
+  
+  server.use("/public",express.static(path.join(__dirname, "../front-end/public/")));
+  
+  // server.use(function (req, res, next) {
+  //   req.headers['content-type'] = 'application/json';
+  //   next();
+  // });
+  // use the above to change the request header for incoming post requests
+  
+  server.use(express.json());
+  server.use(express.urlencoded({ extended: true }))
 
-server.use("/public",express.static(path.join(__dirname, "../front-end/public/")));
+// server.post('/', (req, res) => {
+//   //req.header("Content-Type", "application/json")
+//   console.log('body: ', req.body);
+//   console.log(req.body.firstname)
+// })
 
-server.post('/', (req, res) => {
-    console.log(req.body)
+server.post('/', upload.none(), (req, res) => {
+  //parse the json received in the body and console log the received information
+  // pay special attention to: 
+  // req.on('data', (data) => {
+  //   console.log(data.toString());
+  // });
+    console.log(req.body);
     res.send("welcome")
   });
 
--- front-end/public/script.js
+++ front-end/public/script.js
@@ -5,22 +5,44 @@ function loadEvent() {
     const rootElement = document.getElementById("root");
     
     rootElement.insertAdjacentHTML("beforeend", `
-        <button id="send">GO POST</button>
+    <form>
+        <input type="text" placeholder="First name" class="input" name="firstname"/>
+        <input type="text" placeholder="Last name" class="input" name="lastname"/>
+        <input type="email" placeholder="Email address" class="input" name="email"/>
+        <textarea placeholder="message" class="input" name="message"></textarea>
+        <button id="send" type="submit">Send</button>
+    </form>    
     `)
 
     function postToClick(e) {
+        e.preventDefault();
+        
+        const inputs = e.target.querySelectorAll(".input"), values = {};
+
+        for (const input of inputs) {
+
+            values[`${input.name}`] = input.value;
+        }
+        
+//        console.log(values);
+        const fd = new FormData();
+        fd.append("json", JSON.stringify(values)); //need this because our goal is to append a file to this as well ultimately
+
         fetch('/', {
                 method: 'Post',
                 headers: {
-                    'Content-Type': 'application/json'
+                    //'Content-Type': 'application/json'
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                    //'Content-Type': 'multipart/form-data'
                 },
-                body: "hello"
+                //body: fd
+                body: JSON.stringify(values)
         })
         .then(response => response.text())
         .then(data => console.log(data));
     };
 
-    document.getElementById("send").addEventListener("click", postToClick);
+    window.addEventListener("submit", postToClick);
 
 
 }
